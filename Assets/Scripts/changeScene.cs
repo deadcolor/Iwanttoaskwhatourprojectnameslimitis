80dd33deb1f86db4f2a78feee056b6061128bdf5
deadcolor <sg01257@naver.com>
2015-11-01 19:40:22 +0900

    Connect Scenes
    Add Scene, Add textures, Add fonts and so on

---
+++ Assets/Scripts/changeScene.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeScene : MonoBehaviour {
+
+    public void goTointroScene()
+    {
+        Application.LoadLevel(0);
+    }
+
+    public void goToGameModeSelect()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void goToGameScene()
+    {
+        Application.LoadLevel(2);
+    }
+
+    public void goToScoreScene()
+    {
+        Application.LoadLevel(3);
+    }
+}

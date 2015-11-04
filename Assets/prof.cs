9846a2f7ed31395425d03da6a71eb2a60492da49
yukikaai <shyon25@naver.com>
2015-11-04 16:07:44 +0900

    prof-prefab
    Add prof.cs, prof.prefab

---
+++ Assets/prof.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class prof : MonoBehaviour
+{
+
+    public float recallTime;
+    public bool isActive;
+    public string profName;
+    public int breadType;
+
+    void Start()
+    {
+        isActive = false;
+        profName = "";
+        breadType = 0;
+        recallTime = 0;
+    }
+    void Update()
+    {
+        if (isActive)
+        {
+            recallTime -= Time.deltaTime;
+            if (recallTime < 0)
+            {
+                recallTime = 0;
+                goHome();
+            }
+        }
+    }
+    public void load(string name, int breadType, int recallTime)
+    {
+        this.profName = name;
+        this.breadType = breadType;
+        this.recallTime = recallTime;
+    }
+    public void goHome()
+    {
+
+    }
+}

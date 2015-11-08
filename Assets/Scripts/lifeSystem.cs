74c4dc8e1e65e19fd1161b8f83ed71a79db3d098
deadcolor <sg01257@naver.com>
2015-11-08 19:42:52 +0900

    Merge November first
    LifeSystem, Prof Prefab, Timer, List Create

---
+++ Assets/Scripts/lifeSystem.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lifeSystem : MonoBehaviour {
+
+	public int maxLife;
+
+	public int life;
+
+	public GameObject lifeCell;
+
+	public Sprite empty;
+	public Sprite filled;
+
+	// Use this for initialization
+	void Start () {
+		//life = maxLife;
+		for (int i = 0; i < life; ++i)
+		{
+			GameObject tmp = GameObject.Instantiate(lifeCell);
+			tmp.transform.SetParent(GameObject.Find("LifeIndicator").transform);
+			tmp.transform.localPosition = new Vector3(i * 50.0f, 0.0f, 0.0f);
+			tmp.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+		}
+		for (int i = life; i < maxLife; ++i)
+		{
+			GameObject tmp = GameObject.Instantiate(lifeCell);
+            tmp.transform.SetParent(GameObject.Find("LifeIndicator").transform);
+            tmp.transform.localPosition = new Vector3(i * 50.0f, 0.0f, 0.0f);
+			tmp.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            tmp.GetComponent<UnityEngine.UI.Image>().sprite = empty;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void lifeDecrease()
+	{
+        --life;
+        GameObject lifeBar = GameObject.Find("LifeIndicator");
+        GameObject cell = lifeBar.transform.GetChild(life).gameObject;
+
+        cell.GetComponent<UnityEngine.UI.Image>().sprite = empty;
+        if (life == 0)
+        {
+            // dead
+            GameObject.Find("SceneManager").GetComponent<changeScene>().goToScoreScene();
+        }
+	}
+}

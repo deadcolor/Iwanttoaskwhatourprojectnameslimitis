844eae314ec84123162e5f730baf386fc0bcbb7c
psm918 <psm918@postech.ac.kr>
2015-11-14 09:03:12 +0900

    박성민 바꾼것-11.14
    점수가 변할수 있도록, 그 점수가 끝나고 점수 화면에 뜰 수 있도록. 아직까지는 점수가 오르는 상황이 없기 때문에 그냥 한
    프레임마다 점수가 1씩오르도록 바꿔서 점수가 오르는 것을 확인했고 이를 게임 오버시에 출력하도록 함.
    내용이 살짝 바뀐 파일-timerhandler.cs
    새로 생긴 파일-changescore.cs,scoreoutput.cs

---
+++ Assets/Scripts/changescore.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class changescore : MonoBehaviour {
+    public static int score;
+
+    Text text;
+
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+        score = 0;
+        text.text = " "+score;
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    public void increasescore(int number)
+    {
+        score += number;
+        text.text = " "+score;
+    }
+}
--- Assets/Scripts/timerhandler.cs
+++ Assets/Scripts/timerhandler.cs
@@ -3,14 +3,19 @@ using System.Collections;
 using UnityEngine.UI;
 public class timerhandler : MonoBehaviour {
     public Image timerbar;
-	// Use this for initialization
+    GameObject a;
+    changescore controlscore;
+    // Use this for initialization
 	void Start () {
         timerbar.fillAmount = 0;
-	}
+        a = GameObject.Find("SCORENUMBER");
+        controlscore = a.GetComponent<changescore>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        timerbar.fillAmount += Time.deltaTime/60;
+        timerbar.fillAmount += Time.deltaTime/10;
+        controlscore.increasescore(1);
         if (timerbar.fillAmount == 1)
         {
             print("Game Over");
